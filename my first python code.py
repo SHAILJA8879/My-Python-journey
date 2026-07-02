cf445b36a568415cf2fff65d2ae70448b8012d90
SHAILJA8879 <shailja.141106@gmail.com>
2026-07-03 00:15:21 +0530

    My first code in python

---
+++ my first python code.py
@@ -0,0 +1,4 @@
+print("This is the my beginnig stage of my python coding")
+print("I will try to stay consistent and keep learning and keep growing ")
+print("This journey is started from 1-07-2026")
+print ("Thank you")
