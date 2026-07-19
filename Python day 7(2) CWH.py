093e3c2623f9a32192211740b94d0aea76c0b5f0
SHAILJA8879 <shailja.141106@gmail.com>
2026-07-20 00:35:19 +0530

    Initial commit

---
+++ Python day 7(2) CWH.py
@@ -0,0 +1,19 @@
+print(5+8)
+print(9-1)
+print(5*8)
+print(45/8)
+print(45//8)
+print(45%8)
+print(7**3)
+
+
+#MAKE A SIMPLE CALCULATOR
+a=25
+b=5
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Floor division:",a//b)
+print("Modulus:",a%b)
+print("Exponent:",a**b)
