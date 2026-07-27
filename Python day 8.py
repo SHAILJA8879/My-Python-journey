91a4b2c8b509380ae8459a20e662562ec656bf85
SHAILJA8879 <shailja.141106@gmail.com>
2026-07-28 00:36:34 +0530

    Initial commit

--- Python day 8.py
+++ Python day 8.py
@@ -6,10 +6,18 @@ print(int(a)+int(b))
 #EXPLICIT TYPECASTING
 #WHERE PROGRAMMER TYPECAST BY THERE OWN
 
-#IMPLICIT TYPECASTIN
+#IMPLICIT TYPECASTING
 #WHERE THE PROGRAM TYPECAST BY THERE OWN
 
 c=1.9
-d=8
+d=10
 
 print(c+d)
+
+p=789654
+q=789632541
+print(p+q)
+print(p-q)
+print(p*q)
+print(p/q)
+print(p//q)
