ab293edbf461b4a624a9229218584fcc176390d6
SHAILJA8879 <shailja.141106@gmail.com>
2026-08-04 01:47:26 +0530

    Initial commit

---
+++ python day 11.py
@@ -0,0 +1,17 @@
+#Today i learned about String and how to use it in python language
+
+name='Harry'
+friend='Bella'
+anotherFriend ='Kokonana'
+apple='''He want to eat apple'
+Hi I am your good trainer 
+Have fun with your learning journey'''
+
+print('Hello Good Morning ' + name +  'and'  + anotherFriend)
+print(apple)
+print(name[1])
+print(friend[2])
+
+
+for character in apple:
+    print(character)
