ff0d7f5050da1e262c66d6ce25495ba5a0cb8d6d
SHAILJA8879 <shailja.141106@gmail.com>
2026-08-05 00:40:51 +0530

    Initial commit

---
+++ python day 12.py
@@ -0,0 +1,13 @@
+names="EricaOrlando,Diyuca"
+print(names[0:6])
+Friend="Harrypotter"
+print(len(Friend))
+
+
+fruits="Mango"
+len1=len(fruits)
+print("Mango is a", len1, "letter word.")
+print(fruits[2:4])
+print(fruits[0:3])
+print(fruits[0:-4])
+print(fruits[-4:-1]) 
